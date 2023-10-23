6678c191e175fe4674cbb8c9cc6883a359898628
felixtoo48 <felixtoo48@gmail.com>
2023-10-23 23:07:13 +0300

    deletion

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Get deletion-resilient hypermedia pagination information
+        for a specific index.
+
+        Args:
+            index (int): The start index for the return
+            page (default is None).
+            page_size (int): The number of items per page (default is 10).
+
+        Returns:
+            dict: A dictionary containing deletion-resilient hypermedia
+            pagination information.
+        """
+        assert page_size > 0, "Page size must be greater than 0."
+
+        indexed_data = self.indexed_dataset()
+        if index is None:
+            index = 0
+        assert index >= 0 and index < len(indexed_data),
+
+        current_page_data = []
+        for i in range(index, index + page_size):
+            if i in indexed_data:
+                current_page_data.append(indexed_data[i])
+
+        next_index = index + len(current_page_data)
+
+        hyper_data = {
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(current_page_data),
+            "data": current_page_data,
+        }
+
+        return hyper_data
