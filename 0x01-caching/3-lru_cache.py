6a1c5b7c921e564ac4ee7c18f6a22127328df830
felixtoo48 <felixtoo48@gmail.com>
2023-10-25 21:00:51 +0300

    pyc

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" FIFO cathing method/ caching system"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ FIFO caching system class definition """
+    def __init__(self):
+        """ initializing the class and parent class """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ assigns to the dictionary self.cache_data
+        the item value for the key 'key'
+        if number of items is higher than 'MAX_ITEMS'
+        then remove the first in cache
+        """
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discard = self.order.pop()
+            del self.cache_data[discard]
+            print(f"DISCARD: {discard}")
+
+        if key is not None and item is not None:
+            self.order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ returns the value in the dictionary 'self.cache_data'
+        linked to 'key' """
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
