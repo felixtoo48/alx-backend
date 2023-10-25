b6d07dbf22bb2a8c797a2edbaef9b1eaae90eda4
felixtoo48 <felixtoo48@gmail.com>
2023-10-25 21:24:32 +0300

    updating LRU

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ LRUCache cathing method/ caching system"""
+from collections import OrderedDict
 
 
 BaseCaching = __import__('base_caching').BaseCaching
@@ -11,15 +12,19 @@ class LRUCache(BaseCaching):
         """ initializing the class and parent class """
         super().__init__()
         self.order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """ assigns to the dictionary self.cache_data
         the item value for the key 'key'
         if number of items is higher than 'MAX_ITEMS'
-        then remove the first in cache
+        then remove the last recently used item in cache
         """
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            discard = self.order.pop()
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            discard = self.cache_data.popitem(last=False)
             del self.cache_data[discard]
             print(f"DISCARD: {discard}")
 
