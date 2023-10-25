4f533283d7f1a24cd4cfa3722a7a888a8cfc0c5c
felixtoo48 <felixtoo48@gmail.com>
2023-10-25 17:31:03 +0300

    defining put and get methods for caching from cache dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" BasicCache class inheriting from BaseCaching"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class inheriting from BaseCaching
+    my Cache
+    """
+
+    def put(self, key, item):
+        """ assigns to the dictionary self.cache_data
+        the item value for the key 'key'"""
+        if key or item is None:
+            return
+
+        self.cache_data[key] = {item}
+
+    def get(self, key):
+        """ returns the value in the dictionary 'self.cache_data'
+        linked to 'key' """
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
