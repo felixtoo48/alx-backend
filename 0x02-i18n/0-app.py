caa8de8d88f18979ab239105a4208f566fcbfe80
felixtoo48 <felixtoo48@gmail.com>
2023-10-31 17:25:03 +0300

    updating syntax error

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,14 +2,15 @@
 """ Flask app """
 
 from flask import Flask, render_template
-from app import app
 
 app = Flask(__name__)
 
 
 @app.route('/')
-# @app.route('/index')
-
 def index():
     """ index defin returning render template"""
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
