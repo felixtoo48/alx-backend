554e89f981ece2e43c256e4efdf07c391c3b53b9
felixtoo48 <felixtoo48@gmail.com>
2023-10-31 21:41:45 +0300

    updating style

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,14 +19,14 @@ babel = Babel(app)
 
 def get_locale():
     """ defining accepted languages"""
-    # Check if the locale parameter is present in the request's query parameters
+    # Check if locale parameter is present in the request's query parameters
     requested_locale = request.args.get('locale')
 
     # Check if the requested locale is in the list of supported languages
     if requested_locale and requested_locale in app.config['LANGUAGES']:
         return requested_locale
 
-    # If no locale parameter or an unsupported locale is provided, 
+    # If no locale parameter or an unsupported locale is provided
     # fall back to the previous behavior
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
