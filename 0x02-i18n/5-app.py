b850ef460ed47bbbc344f52691cd556c773d53f8
felixtoo48 <felixtoo48@gmail.com>
2023-10-31 22:56:45 +0300

    simulating mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask app """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -17,7 +17,15 @@ class Config(object):
 babel = Babel(app)
 
 
-@babel.localeselector
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+# @babel.localeselector
 def get_locale():
     """ defining accepted languages"""
     # Check if locale parameter is present in the request's query parameters
@@ -32,17 +40,32 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# babel.init_app(app, locale_selector=get_locale)
+babel.init_app(app, locale_selector=get_locale)
 
 
 # Set the app's config to use the Config class
 app.config.from_object(Config)
 
 
+def get_user():
+    """returns user dictionary or None if ID cannot be found """
+    login_id = request.args.get("login_as")
+
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """ run before request """
+    g.user = get_user()
+
+
 @app.route('/')
 def index():
     """ index definition returning render template"""
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
